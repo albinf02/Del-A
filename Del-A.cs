7f0dc6625e90deca966ea6061cf4348827e16ea8
albinafk <fredrikssonalbin02@hotmail.com>
2026-09-17 18:09:40 +0200

    gör min mapp, och .cs fil. lägger till names och prices.

---
+++ Del-A.cs
@@ -0,0 +1,9 @@
+List<string> names = new List<string>();
+List<int> prices = new List<int>();
+//innehåller namn och listor
+
+names.Add("Ost");
+prices.Add(89);
+
+Console.WriteLine(names[0]);
+Console.WriteLine(prices[0]);
