d7afc46633f33d032667c2d691d6d41813c86108
albinafk <fredrikssonalbin02@hotmail.com>
2026-09-17 18:24:11 +0200

    // frågar användaren att plocka en vara

--- Del-A.cs
+++ Del-A.cs
@@ -14,3 +14,9 @@ for (int i = 0; i < names.Count; i++)
 }
 
 Console.WriteLine($"Totalt: {total} kr");
+
+
+// frågar användaren att plocka en vara
+Console.Write("Skriv en vara: " );
+string name = Console.ReadLine()!;
+
